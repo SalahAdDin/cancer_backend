8c16b915f1ed8c56266b596274b4811606d0ad8d
Luis Alaguna <alagunasalahaddin@live.com>
2021-05-11 18:05:32 +0300

    :tada: Building models.

---
+++ api/result/config/schema.graphql.js
@@ -0,0 +1,18 @@
+module.exports = {
+  query: `
+    resultsCount(where: JSON): Int!
+  `,
+  resolver: {
+    Query: {
+      resultsCount: {
+        description: "Return the count of survey result",
+        resolverOf: "application::result.result.count",
+        resolver: async (obj, options, ctx) => {
+          return await strapi.api.result.services.result.count(
+            options.where || {}
+          );
+        },
+      },
+    },
+  },
+};
