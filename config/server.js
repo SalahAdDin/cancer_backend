fe121a623f55ebc0b48340f10f561a2da22e5f67
Luis Alaguna <alagunasalahaddin@live.com>
2021-05-22 14:58:47 +0300

    :fire: putting settings on the correct place.

--- config/server.js
+++ config/server.js
@@ -5,6 +5,6 @@ module.exports = ({ env }) => ({
     auth: {
       secret: env("ADMIN_JWT_SECRET", "b2ee64303f63047879f1e5799b51682b"),
     },
+    watchIgnoreFiles: ["**/config-sync/files/**"],
   },
-  watchIgnoreFiles: ["**/config-sync/files/**"],
 });
