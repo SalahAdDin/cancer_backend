439ca83f96a9a2a2bc801c763faf4a47f1f12eb4
Luis Alaguna <alagunasalahaddin@live.com>
2021-07-06 14:41:47 +0300

    :fire: ensuring post author.

--- api/post/controllers/post.js
+++ api/post/controllers/post.js
@@ -1,8 +1,57 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
+
+  async create(ctx) {
+    let entity;
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      data.author = ctx.state.user.id;
+      entity = await strapi.services.post.create(data, { files });
+    } else {
+      ctx.request.body.author = ctx.state.user.id;
+      entity = await strapi.services.post.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.post });
+  },
+  /**
+   * Update a record.
+   *
+   * @return {Object}
+   */
+  async update(ctx) {
+    const { id } = ctx.params;
+
+    let entity;
+
+    const [post] = await strapi.services.post.find({
+      id: ctx.params.id,
+      "author.id": ctx.state.user.id,
+    });
+
+    if (!post) {
+      return ctx.unauthorized(`You can't update this entry`);
+    }
+
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.post.update({ id }, data, {
+        files,
+      });
+    } else {
+      entity = await strapi.services.post.update({ id }, ctx.request.body);
+    }
+
+    return sanitizeEntity(entity, { model: strapi.models.post });
+  },
+};
--- api/post/models/post.js
+++ api/post/models/post.js
@@ -1,8 +1,25 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
-module.exports = {};
+module.exports = {
+  /**
+   * It throws and error if the admin does not add an author for the post.
+   */
+  lifecycles: {
+    async beforeCreate(data) {
+      if (data.author == null) {
+        throw strapi.errors.badRequest("This post needs an author.");
+      }
+    },
+    async beforeUpdate(params, data) {
+      if (data.author == null) {
+        throw strapi.errors.badRequest("This post needs an author.");
+      }
+    },
+  },
+};
