8b21d7fcc0b354f0908ce39a191c73bbc6a8276b
Luis Alaguna <alagunasalahaddin@live.com>
2022-03-05 20:13:48 +0300

    :tada: handling messages and token on backend

--- api/post/models/post.js
+++ api/post/models/post.js
@@ -5,6 +5,7 @@
  * to customize this model
  */
 const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
+const { sendNotificationToTopic } = require("../../../utils/notifications");
 
 module.exports = {
   /**
@@ -14,6 +15,23 @@ module.exports = {
     async beforeCreate(data) {
       if (data.author == null) {
         throw strapi.errors.badRequest("This post needs an author.");
+      }
+    },
+    async afterCreate(result, data) {
+      const { type, author, title } = result;
+      try {
+        await sendNotificationToTopic({
+          topic: "posts",
+          data: {
+            type,
+          },
+          notification: {
+            title,
+            body: `${author} has published a new post for ${type}!`,
+          },
+        });
+      } catch (error) {
+        console.log("Error after sending a push notification: ", error);
       }
     },
   },
--- extensions/users-permissions/models/User.js
+++ extensions/users-permissions/models/User.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const { sendNotificationToDevice } = require("../../../utils/notifications");
+
 module.exports = {
   lifecycles: {
     async afterCreate(result, data) {
@@ -18,11 +20,11 @@ module.exports = {
         await strapi
           .query("user", "users-permissions")
           .update({ id: result.id }, { confirmed: false });
-        const admins = await strapi.query("user", "admin").find({ id: 1 });
+        const admins = await strapi.query("user", "admin").findOne({ id: 1 });
         await strapi.plugins["email"].services.email.sendTemplatedEmail(
           {
             // First admin user should be the main one, at first.
-            to: admins[0].email,
+            to: admins.email,
           },
           emailTemplate,
           {
@@ -45,7 +47,9 @@ module.exports = {
       try {
         const deletedUser = result[0];
         const profileId = deletedUser.profile.id;
+        const uid = deletedUser.profile.uid;
         await strapi.services.profile.delete({ id: profileId });
+        await strapi.firebase.auth().deletedUser(uid);
 
         await strapi.plugins["email"].services.email.sendTemplatedEmail(
           {
@@ -63,22 +67,35 @@ module.exports = {
     async afterUpdate(result, params, data) {
       const emailTemplate = {
         subject: "Welcome <%= user.username %>",
-        text: `Welcome on TODO: !
+        text: `Welcome on JineOnkolojik Destek !
           Your account is confirmed with: <%= user.email %>.`,
-        html: `<h1>Welcome onTODO !</h1>
+        html: `<h1>Welcome on JineOnkolojik Destek !</h1>
           <p>Your account is now confirmed with: <%= user.email %>.<p>`,
       };
+      const { email, username } = result;
 
       try {
         // This is supposed to occur once.
         if (result.confirmed === true) {
+          const { token } = result.profile;
+
+          console.log("result profile: ", result.profile);
+
+          await sendNotificationToDevice({
+            token,
+            notification: {
+              title: `Welcome ${username}!`,
+              body: `Your account is confirmed with: ${email}.`,
+            },
+          });
+
           await strapi.plugins["email"].services.email.sendTemplatedEmail(
             {
-              to: result.email,
+              to: email,
             },
             emailTemplate,
             {
-              user: { username: result.username, email: result.email },
+              user: { username, email },
             }
           );
         }
